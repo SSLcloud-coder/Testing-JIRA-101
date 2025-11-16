f775246e545783d9d685746de30098eef39c1dd9
ssl-cloud <sslcloudlearning@gmail.com>
2025-11-16 15:58:04 +0530

    Vnet Added feture/JIRA-101

--- main.tf
+++ main.tf
@@ -9,6 +9,14 @@ resource "azurerm_virtual_network" "vnet" {
   location           = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
+
+#VNET added -JIRA-101
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.prefix}-vnet"
+  address_space       = ["10.0.0.7/44"]
+  location           = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
 resource "azurerm_subnet" "subnet" {
   name                 = "${var.prefix}-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
